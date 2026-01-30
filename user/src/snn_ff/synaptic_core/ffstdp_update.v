5889289c7d9db5c77aed8c0f2d87ae547cb1f51f
Sephiroth <1245598043@qq.com>
2026-01-30 15:15:08 +0800

    adapt to relu ff; goodness gradient to be fixed

--- user/src/snn_ff/synaptic_core/ffstdp_update.v
+++ user/src/snn_ff/synaptic_core/ffstdp_update.v
@@ -23,78 +23,74 @@ module ffstdp_update #(
     output reg signed [GRAD_WIDTH-1:0] GRAD_NEW
 );
 
-    localparam max_value = (1 << (WEIGHT_WIDTH-1)) - 1;
-    localparam min_value = -(1 << (WEIGHT_WIDTH-1));
+    localparam weight_max_value = (1 << (WEIGHT_WIDTH-1)) - 1;
+    localparam weight_min_value = -(1 << (WEIGHT_WIDTH-1));
+    localparam grad_max_value = (1 << (GRAD_WIDTH-1)) - 1;
+    localparam grad_min_value = -(1 << (GRAD_WIDTH-1));
+    localparam scale = 4;
 
+    // ReLU * spike item
+    wire [$clog2(POST_CNT_WIDTH):0] pre_spike_valid_sum;
+    adder_tree #(
+        .N     	(POST_CNT_WIDTH),
+        .WIDTH 	(1)
+    )
+    u_adder_tree(
+        .in  	(PRE_SPIKE_CNT & POST_SPIKE_CNT),
+        .out 	(pre_spike_valid_sum)
+    );
+
+    //------------CLK 1--------------------
+    // Positive derivative
+    wire signed [GRAD_WIDTH-1:0] L_to_s_derivative_pos = pre_spike_valid_sum * AVG_GOODNESS;
+    // Negative derivative
+    wire signed [GRAD_WIDTH-1:0] L_to_s_derivative_neg = pre_spike_valid_sum * (1'b1 - AVG_GOODNESS);
+    // SRAM read delay registers
     reg signed [WEIGHT_WIDTH-1:0] WSYN_CURR_reg;
     reg signed [GRAD_WIDTH-1:0] GRAD_CURR_reg;
-    reg signed [WEIGHT_WIDTH-1:0] delta_w_signed_reg;
-
-
-    wire [7:0] rom_address;
-    wire [POST_CNT_WIDTH-1:0]       POST_SPIKE_CNT_encoded;
-    wire [PRE_CNT_WIDTH-1:0]        PRE_SPIKE_CNT_encoded;
-    wire POST_SPIKE_CNT_eq_zero;
-    wire PRE_SPIKE_CNT_eq_zero;
-
-    wire signed [WEIGHT_WIDTH-1:0] L_to_s_derivative_pos; //Q0.8
-    wire signed [WEIGHT_WIDTH-1:0] L_to_s_derivative_neg; //Q0.8
-    wire signed [WEIGHT_WIDTH-1:0] L_to_s_derivative; //Q1.7
-
-    wire signed [WEIGHT_WIDTH-1:0] delta_w_signed; //Q3.4
-    wire signed [WEIGHT_WIDTH-1:0] new_w_result;   //Q3.4
-
-
-    assign PRE_SPIKE_CNT_encoded = PRE_SPIKE_CNT - 1'b1;
-    assign POST_SPIKE_CNT_encoded = POST_SPIKE_CNT - 1'b1;
-    assign rom_address =  {PRE_SPIKE_CNT_encoded[3:0],POST_SPIKE_CNT_encoded[3:0]};
-
-    assign PRE_SPIKE_CNT_eq_zero = !(|PRE_SPIKE_CNT);
-    assign POST_SPIKE_CNT_eq_zero = !(|POST_SPIKE_CNT);
-    assign L_to_s_derivative = (IS_POS)? L_to_s_derivative_pos : L_to_s_derivative_neg;
-    assign delta_w_signed = (PRE_SPIKE_CNT_eq_zero || POST_SPIKE_CNT_eq_zero)? 'd0 : (L_to_s_derivative);
-    assign new_w_result = WSYN_CURR_reg + delta_w_signed_reg;// 权重值同为Q3.4，其中1位符号位，3位整数位，4位小数位，
-
-    assign overflow = !(WSYN_CURR_reg[WEIGHT_WIDTH-1] ^ delta_w_signed_reg[WEIGHT_WIDTH-1]) && (new_w_result[WEIGHT_WIDTH-1] ^ WSYN_CURR_reg[WEIGHT_WIDTH-1]);
     always @(posedge CLK)           
     begin                                        
         WSYN_CURR_reg <= WSYN_CURR;
-        GRAD_CURR_reg <= GRAD_CURR;
-        delta_w_signed_reg <= delta_w_signed;                          
+        GRAD_CURR_reg <= GRAD_CURR;                        
     end 
-    wire signed [WEIGHT_WIDTH-1:0] w_slect =  overflow? 
-                                            (new_w_result[WEIGHT_WIDTH-1] == 1'b1)? max_value : min_value 
-                                            : new_w_result;                                        
+    //------------CLK 2--------------------
+    // Select derivative based on IS_POS
+    wire signed [GRAD_WIDTH-1:0] L_to_s_derivative = (IS_POS)? L_to_s_derivative_pos : L_to_s_derivative_neg;
+    // Gradient accumulation
+    wire signed [GRAD_WIDTH-1:0] GRAD_ACCUM = GRAD_CURR_reg + (L_to_s_derivative >>> scale);
+    // New weight result
+    wire signed [WEIGHT_WIDTH-1:0] new_w_result = WSYN_CURR_reg + GRAD_ACCUM;
+    // Overflow detection for weight and gradient
+    wire weight_overflow = !(WSYN_CURR_reg[WEIGHT_WIDTH-1] ^ GRAD_ACCUM[GRAD_WIDTH-1]) && (new_w_result[WEIGHT_WIDTH-1] ^ WSYN_CURR_reg[WEIGHT_WIDTH-1]);
+    wire grad_overflow = !(GRAD_CURR_reg[GRAD_WIDTH-1] ^ L_to_s_derivative[GRAD_WIDTH-1]) && (GRAD_ACCUM[GRAD_WIDTH-1] ^ GRAD_CURR_reg[GRAD_WIDTH-1]);
+
+    // Select final weight and gradient values considering overflow
+    wire signed [WEIGHT_WIDTH-1:0] weight_slect =  weight_overflow? 
+                                            (new_w_result[WEIGHT_WIDTH-1] == 1'b1)? weight_max_value : weight_min_value 
+                                            : new_w_result;   
+    wire signed [GRAD_WIDTH-1:0] grad_slect =  grad_overflow? 
+                                            (GRAD_ACCUM[GRAD_WIDTH-1] == 1'b1)? grad_max_value : grad_min_value 
+                                            : GRAD_ACCUM;
+
+                                         
 	always @(*) begin
+        // Update weight during training and enable training
 		if (CTRL_TREF_EVENT && IS_TRAIN) begin 
-            WSYN_NEW = w_slect;
-            GRAD_NEW = 1'b1;
+            WSYN_NEW = weight_slect;
         end
         else begin
             WSYN_NEW = WSYN_CURR_reg;
+        end
+        // Update gradient only during inference and disable training; Once updating weight, gradient is set to zero
+        if (CTRL_TREF_EVENT && !IS_TRAIN) begin
+            GRAD_NEW = grad_slect;
+        end
+        else if (CTRL_TREF_EVENT && IS_TRAIN) begin
+            GRAD_NEW = 'd0;
+        end
+        else begin
             GRAD_NEW = GRAD_CURR_reg;
-        end   
+        end
 	end 
     
-    // pos_derivative_rom pos_derivative_rom_0(
-    // .a(rom_address),      // input wire [7 : 0] a
-    // .spo(L_to_s_derivative_pos)  // output wire [7 : 0] spo
-    // );
-    // neg_derivative_rom neg_derivative_rom_0(
-    // .a(rom_address),      // input wire [7 : 0] a
-    // .spo(L_to_s_derivative_neg)  // output wire [7 : 0] spo
-    // );
-    pos_derivative_rom u_pos_derivative_rom
-    (
-        .clk                                   (CLK                ),
-        .addr                                  (rom_address        ),
-        .dout                                  (L_to_s_derivative_pos) 
-    );
-    neg_derivative_rom u_neg_derivative_rom
-    (
-        .clk                                   (CLK                ),
-        .addr                                  (rom_address        ),
-        .dout                                  (L_to_s_derivative_neg) 
-    );
-
 endmodule
